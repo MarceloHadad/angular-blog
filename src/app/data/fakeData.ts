b03707afa60a40f4b0fc43687024366ca3f99dad
Marcelo Hadad <marcelo.hadad@outlook.com>
2024-01-06 12:04:34 -0300

    feat: Make the content page dynamic

--- src/app/data/fakeData.ts
+++ src/app/data/fakeData.ts
@@ -1,20 +1,26 @@
 export const fakeData = [
   {
-    "id": 1,
-    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa0%2Fa9%2F3e%2Fa0a93e6577bb210b51918a09793b7ddd.jpg&f=1&nofb=1&ipt=829054e63bfbab8a2f53aa203fef19bda4645be926792eced67daa1af88343e4&ipo=images",
-    "contentTitle": "Fofoca quentinha",
-    "contentDescription": "Aceitas?"
+    "id": "1",
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FmsmI9Nx9QS8%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=df1be256a0caa09e2c8305ab2d43d67c80d92bca1fe9a03fdd87b104cb49d034&ipo=images",
+    "contentTitle": "hmmm bolo de murangu",
+    "contentDescription": ""
   },
   {
-    "id": 2,
-    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.adorocinema.com%2Fpersonagens%2Fpersonagem-12344%2F&f=1&nofb=1&ipt=829054e63bfbab8a2f53aa203fef19bda4645be926792eced67daa1af88343e4&ipo=images",
-    "contentTitle": "O que é isso?",
-    "contentDescription": "O que é isso?"
+    "id": "2",
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fn5n68jNBSlQ%2Fhqdefault.jpg%3Fsqp%3D-oaymwEmCOADEOgC8quKqQMa8AEB-AGKBIAC4AOKAgwIABABGE0gTShlMA8%3D%26rs%3DAOn4CLAgp7tGZ4xbLg3pAYIeLatsFeeKQg&f=1&nofb=1&ipt=d3fe055310231a4747345c19319895ac400a91f3222b7c2ee689ce0132a1346a&ipo=images",
+    "contentTitle": "Escuta o som do meu corsa ai",
+    "contentDescription": ""
   },
   {
-    "id": 3,
-    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.adorocinema.com%2Fpersonagens%2Fpersonagem-12344%2F&f=1&nofb=1&ipt=829054e63bfbab8a2f53aa203fef19bda4645be926792eced67daa1af88343e4&ipo=images",
-    "contentTitle": "O que é isso?",
-    "contentDescription": "O que é isso?"
+    "id": "3",
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimageproxy.ifunny.co%2Fcrop%3Ax-20%2Cresize%3A640x%2Cquality%3A90x75%2Fimages%2F12ef39f3a801013d09416fa8744ee3a0e1795f5059e2c7332c0e5293dce500cf_1.jpg&f=1&nofb=1&ipt=0a415d6f9292ed69b8b0d831c36e76cd496aac9ebc4c1abd11f1035a36c9bb2e&ipo=images",
+    "contentTitle": "Pipoca quente na manteiga",
+    "contentDescription": ""
+  },
+  {
+    "id": "4",
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F24%2F3e%2F19%2F243e19ba35215ed51670e5c9013f748c.jpg&f=1&nofb=1&ipt=4bf610177c4eeca2c5e7a6ae79a55fc25a7741717c4dcf6701c49a0107779746&ipo=images",
+    "contentTitle": "Obrigado amigo, você é um amigo",
+    "contentDescription": ""
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { fakeData } from 'src/app/data/fakeData';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  photoCover:string = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa0%2Fa9%2F3e%2Fa0a93e6577bb210b51918a09793b7ddd.jpg&f=1&nofb=1&ipt=829054e63bfbab8a2f53aa203fef19bda4645be926792eced67daa1af88343e4&ipo=images"
-  contentTitle:string = "Fofoca quentinha"
-  contentDescription:string = "Aceitas?"
+  photoCover: string =
+    '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+  private id: string | null = '';
 
-  constructor(
-    private route: ActivatedRoute
-  ) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
-    )
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+
+    this.setValuesOnComponent(this.id);
   }
 
+  setValuesOnComponent(id: string | null) {
+    const result = fakeData.filter((article) => article.id === id)[0];
+
+    this.contentTitle = result.contentTitle;
+    this.contentDescription = result.contentDescription;
+    this.photoCover = result.photoCover;
+  }
 }
