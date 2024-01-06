2fc9e03d3ec0b465e82f84d0619654dfc949aca1
Marcelo Hadad <marcelo.hadad@outlook.com>
2024-01-06 11:01:29 -0300

    feat: Create additional pages

---
+++ src/app/data/fakeData.ts
@@ -0,0 +1,20 @@
+export const fakeData = [
+  {
+    "id": 1,
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa0%2Fa9%2F3e%2Fa0a93e6577bb210b51918a09793b7ddd.jpg&f=1&nofb=1&ipt=829054e63bfbab8a2f53aa203fef19bda4645be926792eced67daa1af88343e4&ipo=images",
+    "contentTitle": "Fofoca quentinha",
+    "contentDescription": "Aceitas?"
+  },
+  {
+    "id": 2,
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.adorocinema.com%2Fpersonagens%2Fpersonagem-12344%2F&f=1&nofb=1&ipt=829054e63bfbab8a2f53aa203fef19bda4645be926792eced67daa1af88343e4&ipo=images",
+    "contentTitle": "O que é isso?",
+    "contentDescription": "O que é isso?"
+  },
+  {
+    "id": 3,
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.adorocinema.com%2Fpersonagens%2Fpersonagem-12344%2F&f=1&nofb=1&ipt=829054e63bfbab8a2f53aa203fef19bda4645be926792eced67daa1af88343e4&ipo=images",
+    "contentTitle": "O que é isso?",
+    "contentDescription": "O que é isso?"
+  }
+]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  photoCover:string = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa0%2Fa9%2F3e%2Fa0a93e6577bb210b51918a09793b7ddd.jpg&f=1&nofb=1&ipt=829054e63bfbab8a2f53aa203fef19bda4645be926792eced67daa1af88343e4&ipo=images"
+  contentTitle:string = "Fofoca quentinha"
+  contentDescription:string = "Aceitas?"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
